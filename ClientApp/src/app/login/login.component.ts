e55b20e8080d494073cbf61dc0f7697942a973af
unknown <matheussf95@gmail.com>
2019-08-25 16:03:41 -0300

    adicioando redirecionamento à tela de login para acessar a tela home ao logar.

--- ClientApp/src/app/login/login.component.ts
+++ ClientApp/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'login',
@@ -7,12 +8,12 @@ import { HttpClient } from '@angular/common/http';
 })
 export class LoginComponent {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   login(){
     this.http.post('http://localhost:5000/api/Login', {Nome: 'admin', Senha: 'admin'}).subscribe(result => {
-      console.log(result);
       localStorage.setItem('token', result["token"]);
+      this.router.navigate(['/home']); 
     }, error => console.error(error));
   }
 }
